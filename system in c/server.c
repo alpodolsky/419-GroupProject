8a215727dbb6cf41f1159f096990f9035e32aeac
Alex Podolsky <alpodolsky1@yahoo.com>
2020-04-19 23:30:47 -0400

    server was missing some stuff

--- system in c/server.c
+++ system in c/server.c
@@ -38,12 +38,12 @@ int sendMsg(int socfd, char* msg, int msgLength){
 	}
 	return 0;
 }
-int interpretCmd(struct input thisInput, int socfd, struct threadInfo* thisThreadInfo){
+int interpretCmd(char* thisInput, int socfd, struct threadInfo* thisThreadInfo){
 	char* goodbye = "GDBYE";
 	char* hello = "HELLO";
 	char* create = "CHUCK";
 
-    char* cmd = thisInput.cmd;
+    char* cmd = thisInput;
 	if (strcmp(cmd, goodbye) == 0){
 		printf("client called the 'quit' command\n");
 		//call quit fnction
@@ -53,12 +53,12 @@ int interpretCmd(struct input thisInput, int socfd, struct threadInfo* thisThrea
     //can be changed here to accept a key from alice/bob
     else if (strcmp(cmd, hello) == 0){
 		printf("client tried to connect to the server");
-		char helloResponse[] = "Welcome to the server";
+		char helloResponse[3] = "OK\0";
 		sendMsg(socfd, helloResponse, sizeof(helloResponse));
 		return 0;
 	}
     else{ //the  message sent from bob/alice
-		printf("client sent the string: %s\n", thisInput.cmd);
+		printf("client sent the string: %s\n", cmd);
 		//at this point call encryption and do everything necessary
             //includes encrypting the message, storing for referencing by chuck
             //and sending the message to the  recepiant. 
@@ -67,6 +67,27 @@ int interpretCmd(struct input thisInput, int socfd, struct threadInfo* thisThrea
 	}
     //we would need another command/condition for chuck
 }
+
+//reads the command
+char* readCmd(int socfd){
+	printf("  made it into read cmd\n");
+	char *input=malloc(1*sizeof(char));
+	char buff[1];
+	size_t inputlen = 0;
+	do{
+		int bytesRead = 0;
+		bytesRead = read(socfd,buff,1);
+		if (bytesRead == 0){ //server disconected socket
+			printf("The server has disconnected\n");
+			exit(0);
+		}
+		input = realloc(input, inputlen+1);
+		strcpy(input+inputlen,buff);
+		inputlen += 1;
+	} while(buff[0]!='\0');	
+	printf("   Server got sent: %s, %d bytes, %c\n",input, strlen(input)+1,input[strlen(input)-1]);
+	return input;
+}
 int j = 0;
 void* communicate(void* socfd){
     struct threadInfo thisThreadInfo;
@@ -74,7 +95,7 @@ void* communicate(void* socfd){
     int* clientfd = (int*)socfd;
     int client = *clientfd;
     while(1){
-       	struct input thisIn = readInput(client);
+       	char* thisIn = readCmd(client);
 	    int i;
         i = interpretCmd(thisIn, client, &thisThreadInfo);
 		if (i == 1){
