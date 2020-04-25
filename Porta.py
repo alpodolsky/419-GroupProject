09dfccc113749faec238caf5950cbc7327f4f722
pyedlapalli <prasanth@Prasanths-MacBook-Pro.local>
2020-04-25 17:54:31 -0400

    Fixed decryption

--- Porta.py
+++ Porta.py
@@ -7,6 +7,7 @@ def create_table():
     matrix = np.chararray((13, 26))
 
     index = 0
+    index4 = 0
 
     for i in range(len(firsthalf)):
         index2 = index
@@ -19,7 +20,7 @@ def create_table():
             else:
                 index2+=1
         
-        index3 = index
+        index3 = index4
         for j in range(len(secondhalf)):
             letter = firsthalf[index3]
             matrix[i][j+13] = letter
@@ -33,13 +34,16 @@ def create_table():
             index = 0
         else:
             index+=1
+        if index4 == 0:
+            index4 = 12
+        else:
+            index4 -= 1
     
     return matrix
 
 def encrypt(plaintext, key):
     ciphertext = ''
     matrix = create_table()
-    print(matrix)
     plaintext_no_space = plaintext.replace(' ', '')
     keyText = ''
     index = 0
@@ -61,8 +65,6 @@ def encrypt(plaintext, key):
             row = (ord(keyText[index]) - ord('a')) / 2
             column = ord(plaintext_no_space[index]) - ord('a')
 
-            print(f'Row: {row} and column: {column}')
-
             ciphertext += matrix[int(row)][int(column)].decode('utf-8')
             if index == len(keyText)-1:
                 index = 0
@@ -73,8 +75,11 @@ def encrypt(plaintext, key):
 
 def main():
     plaintext = 'defend the walls of the castle'
+    ciphertext = 'synnjs hwy bwpng mz lqv rzdmtn'
     key = 'fortify'
     p = encrypt(plaintext, key)
+    print(p)
+    p = encrypt(ciphertext, key)
     print(p)
 
 main()
